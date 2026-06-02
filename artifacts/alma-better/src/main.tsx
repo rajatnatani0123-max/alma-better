fa0a2573904bace522c269afb2dc434cf1c54fc7
rajatnatani0123-max <rajatnatani0123@gmail.com>
2026-06-02 20:33:56 +0530

    Add Razorpay checkout script to main.tsx

--- artifacts/alma-better/src/main.tsx
+++ artifacts/alma-better/src/main.tsx
@@ -1,3 +1,11 @@
+const razorpayScript = document.createElement("script");
+
+razorpayScript.src = "https://checkout.razorpay.com/v1/checkout.js";
+
+razorpayScript.async = true;
+
+document.body.appendChild(razorpayScript);
+
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
